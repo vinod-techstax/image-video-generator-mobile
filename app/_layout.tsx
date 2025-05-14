1b8a8ba3603ca2eb49270847addf5a79b6dad781
vinod <vinod@techstax.ml>
2025-05-14 20:33:16 +0530

    react native reusable setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,12 @@
 import '@/global.css';
 
-import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from '@react-navigation/native';
+import { Theme, ThemeProvider, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { Platform, SafeAreaView } from 'react-native';
+import { Platform } from 'react-native';
 import { NAV_THEME } from '@/lib/constants';
-import { useColorScheme } from '@/lib/useColorScheme';
-import { PortalHost } from '@rn-primitives/portal';
-import { setAndroidNavigationBar } from '@/lib/android-navigation-bar';
-import Header from '@/components/custom/header';
-import { QueryProvider } from "@/providers/query-provider";
+import { useColorScheme } from '@/lib/use-color-scheme';
 
 const LIGHT_THEME: Theme = {
   ...DefaultTheme,
@@ -28,7 +24,7 @@ export {
 
 export default function RootLayout() {
   const hasMounted = React.useRef(false);
-  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const { isDarkColorScheme } = useColorScheme();
   const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
 
   useIsomorphicLayoutEffect(() => {
@@ -40,7 +36,6 @@ export default function RootLayout() {
       // Adds the background color to the html element to prevent white background on overscroll.
       document.documentElement.classList.add('bg-background');
     }
-    setAndroidNavigationBar(colorScheme);
     setIsColorSchemeLoaded(true);
     hasMounted.current = true;
   }, []);
@@ -50,29 +45,12 @@ export default function RootLayout() {
   }
 
   return (
-    <QueryProvider>
-      <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-        <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
-        <SafeAreaView style={{ flex: 1, paddingTop: 30 }}>
-          <Stack>
-            <Stack.Screen
-              name='index'
-              options={{
-                title: 'Starter Base',
-                header: Header,
-              }}
-            />
-          </Stack>
-          <PortalHost />
-        </SafeAreaView>
-      </ThemeProvider>
-    </QueryProvider>
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
+      <Stack />
+    </ThemeProvider>
   );
 }
 
 const useIsomorphicLayoutEffect =
   Platform.OS === 'web' && typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect;
-
-
-
-
